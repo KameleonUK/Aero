6ba6b8423ab38ac42d21cf5a41746590a7d0c412
KameleonUK <42344512+KameleonUK@users.noreply.github.com>
2025-02-04 11:51:23 +0000

    Extended the recent world map fix to work with the Dragon Flight UI

--- Aero.lua
+++ Aero.lua
@@ -1,155 +1,318 @@
 local _G = getfenv()
 
-local Aero = CreateFrame("Frame")
+--[[
+Aero:RegisterFrames(...)
+Use it to register show/hide animation to frames
+Arguments can be either frame names or hook functions, separated by commas
+]]
 
-local defaultDuration = 0.2
-local duration = defaultDuration
+Aero:RegisterFrames(
+    "GameMenuFrame",
+    "SoundOptionsFrame",
+    "OptionsFrame",
+    "UIOptionsFrame",
+    "OpacityFrame",
+    "ColorPickerFrame",
+    "TabardFrame",
+    "StackSplitFrame",
+    "ItemRefTooltip",
+    "GuildInfoFrame",
+    "ReputationDetailFrame",
+    "HelpFrame",
+    "MailFrame",
+    "OpenMailFrame",
+    "TradeFrame",
+    "CharacterFrame",
+    "BankFrame",
+    "PetStableFrame",
+    "QuestLogFrame",
+    "FriendsFrame",
+    "TaxiFrame",
+    "BattlefieldFrame",
+    "LootFrame"
+)
 
-local animating = {}
-local addons = {}
-
-local function print(msg) DEFAULT_CHAT_FRAME:AddMessage(msg) end
-
-Aero:RegisterEvent("VARIABLES_LOADED")
-Aero:RegisterEvent("ADDON_LOADED")
-Aero:SetScript("OnEvent", function()
-    if event == "VARIABLES_LOADED" then
-        AeroDB = AeroDB or {}
-        AeroDB.duration = AeroDB.duration or defaultDuration
-        duration = AeroDB.duration
-    elseif event == "ADDON_LOADED" then
-        if addons[arg1] then
-            Aero:RegisterFrames(unpack(addons[arg1]))
-            addons[arg1] = nil
-        end
-    end
-end)
-
-local function onShow(frame)
-    local aero = frame.aero
-    aero.animating = true
-
-    tinsert(animating, frame)
-
-    aero.scaleDiff = 0.6
-    aero.startScale = aero.origScale - aero.scaleDiff
+local function moveFrame(frame, point, x, y)
+    frame:ClearAllPoints()
+    frame:SetPoint(point, x, y)
 end
-Aero:SetScript("OnShow", onShow)
 
-local function onHide(frame)
-    local aero = frame.aero
-    aero.animating = true
+local function delayRun(delay, callback)
+    local elapsed = 0
+    local frame = CreateFrame("Frame")
 
-    tinsert(animating, frame)
+    frame:SetScript("OnUpdate", function()
+        elapsed = elapsed + arg1
 
-    aero.startScale = aero.origScale
-    aero.scaleDiff = -0.6
-    aero.finished = true
-
-    frame:Show()
-end
-Aero:SetScript("OnHide", onHide)
-
-Aero:SetScript("OnUpdate", function()
-    for i, frame in next, animating do
-        local aero = frame.aero
-        aero.elapsed = aero.elapsed + arg1
-
-        if aero.elapsed >= duration then
-            aero.elapsed = 0
-            animating[i] = nil
-
-            frame:SetScale(aero.origScale)
-            frame:SetAlpha(aero.origAlpha)
-
-            if aero.finished then
-                aero.finished = false
-                HideUIPanel(frame)
-            end
-
-            aero.animating = false
-        else
-            local progress = aero.elapsed / duration
-            local scale = aero.startScale + aero.scaleDiff * progress
-            scale = scale <= 0 and 0.01 or scale
-
-            local scalePct = scale / aero.origScale
-            local alpha = (scalePct <= 0.3) and 0 or math.min(aero.origAlpha, ((scalePct - 0.3) / 0.7) ^ 4)
-
-            frame:SetAlpha(alpha)
-            frame:SetScale(scale)
+        if elapsed >= delay then
+            callback()
+            frame:SetScript("OnUpdate", nil)
         end
-    end
-end)
+    end)
+end
 
-function Aero:RegisterFrames(...)
-    for i = 1, arg.n do
-        local currentArg = arg[i]
+local function delayHideOnEvent(frame, eventName, delay)
+    delay = delay or 0
+    local origOnEvent = frame:GetScript("OnEvent")
 
-        if type(currentArg) == "string" then
-            local frame = _G[currentArg]
-            if not frame or (frame.aero and frame.aero.registered) then return end
+    frame:SetScript("OnEvent", function()
+        if event == eventName then
+            frame.aero.delayEvent = false
 
-            frame.aero = frame.aero or {}
-            frame.aero.registered = true
-            frame.aero.elapsed = 0
-            frame.aero.origScale = frame:GetScale()
-            frame.aero.origAlpha = frame:GetAlpha()
-
-            local origOnShow = frame:GetScript("OnShow")
-            frame:SetScript("OnShow", function()
-                if frame.aero.animating then return end
-                if origOnShow then origOnShow(frame) end
-                onShow(frame)
-            end)
-
-            local origOnHide = frame:GetScript("OnHide")
-            frame:SetScript("OnHide", function()
-                if frame.aero.animating then return end
-                if origOnHide then origOnHide(frame) end
-                onHide(frame)
+            delayRun(delay, function()
+                if not frame.aero.delayEvent then
+                    if origOnEvent then origOnEvent() end
+                    HideUIPanel(frame)
+                end
             end)
         else
-            currentArg()
+            frame.aero.delayEvent = true
+            origOnEvent()
+        end
+    end)
+end
+
+local function registerFrameAndDelayEvent(frame, eventName, delay)
+    Aero:RegisterFrames(frame)
+    delayHideOnEvent(_G[frame], eventName, delay)
+end
+
+-- Quest, gossip, and merchant frames
+registerFrameAndDelayEvent("QuestFrame", "QUEST_FINISHED")
+registerFrameAndDelayEvent("GossipFrame", "GOSSIP_CLOSED")
+registerFrameAndDelayEvent("MerchantFrame", "MERCHANT_CLOSED")
+
+-- Spellbook
+Aero:RegisterFrames("SpellBookFrame")
+
+local origToggleSpellBook = ToggleSpellBook
+function ToggleSpellBook(bookType)
+    this = this or SpellBookFrame
+
+    if SpellBookFrame:IsVisible() and SpellBookFrame.bookType ~= bookType then
+        SpellBookFrame.bookType = bookType
+        SpellBookFrame_Update(1)
+        return
+    end
+
+    origToggleSpellBook(bookType)
+end
+
+-- Backpack and bags
+for i = 1, NUM_CONTAINER_FRAMES do
+    Aero:RegisterFrames("ContainerFrame" .. i)
+end
+
+local function updateChecked(buttonID)
+    local translatedID = 0
+    if buttonID ~= 0 then translatedID = buttonID - CharacterBag0Slot:GetID() + 1 end
+
+    local isVisible = 0
+    for i = 1, NUM_CONTAINER_FRAMES do
+        local frame = _G["ContainerFrame" .. i]
+        if frame:GetID() == translatedID and frame:IsVisible() and not frame.aero.finished then
+            isVisible = 1
+            break
         end
     end
+    this:SetChecked(isVisible)
 end
 
-function Aero:RegisterAddon(addon, ...)
-    if IsAddOnLoaded(addon) then
-        for i = 1, arg.n do
-            Aero:RegisterFrames(arg[i])
+local origBagSlotButton_OnClick = BagSlotButton_OnClick
+function BagSlotButton_OnClick()
+    origBagSlotButton_OnClick()
+    updateChecked(this:GetID())
+end
+
+local origBagSlotButton_OnDrag = BagSlotButton_OnDrag
+function BagSlotButton_OnDrag()
+    origBagSlotButton_OnDrag()
+    updateChecked(this:GetID())
+end
+
+local origBagSlotButton_OnShiftClick = BagSlotButton_OnShiftClick
+function BagSlotButton_OnShiftClick()
+    origBagSlotButton_OnShiftClick()
+    updateChecked(this:GetID())
+end
+
+local origBackpackButton_OnClick = BackpackButton_OnClick
+function BackpackButton_OnClick()
+    origBackpackButton_OnClick()
+    updateChecked(0)
+end
+
+local origUpdateContainerFrameAnchors = updateContainerFrameAnchors
+function updateContainerFrameAnchors() delayRun(0, origUpdateContainerFrameAnchors) end
+
+local origOpenAllBags = OpenAllBags
+function OpenAllBags(forceOpen)
+    if not UIParent:IsVisible() then return end
+
+    local bagsOpen, totalBags = 0, 1
+
+    for i = 1, NUM_CONTAINER_FRAMES do
+        local frame = _G["ContainerFrame" .. i]
+        local button = _G["CharacterBag" .. (i - 1) .. "Slot"]
+
+        if button then
+            local bagID = button:GetID() - _G["CharacterBag0Slot"]:GetID() + 1
+
+            if i <= NUM_BAG_FRAMES and GetContainerNumSlots(bagID) > 0 then totalBags = totalBags + 1 end
         end
-    else
-        local _, _, _, enabled = GetAddOnInfo(addon)
-        if enabled then
-            addons[addon] = {}
-            for i = 1, arg.n do
-                tinsert(addons[addon], arg[i])
-            end
+
+        if frame:IsShown() then
+            frame.aero.animating = true
+            if frame:GetID() ~= KEYRING_CONTAINER then bagsOpen = bagsOpen + 1 end
         end
     end
-end
 
-function Aero:SetDuration(newDuration)
-    if newDuration and newDuration >= 0 then
-        AeroDB.duration = newDuration
-        duration = newDuration
-        return true
+    if bagsOpen >= totalBags and not forceOpen then
+        for i = 1, NUM_CONTAINER_FRAMES do
+            _G["ContainerFrame" .. i].aero.animating = false
+        end
     end
-    return false
+
+    origOpenAllBags(forceOpen)
 end
 
-SLASH_AERO1 = "/aero"
-SlashCmdList["AERO"] = function(msg)
-    local newDuration = tonumber(msg)
+local origToggleBag = ToggleBag
+function ToggleBag(id)
+    local frame = _G["ContainerFrame" .. id]
 
-    if Aero:SetDuration(newDuration) then
-        local secondText = (newDuration == 1) and "second" or "seconds"
-        print("Aero duration set to " .. newDuration .. " " .. secondText .. ".")
-    else
-        print("Usage: /aero <duration in seconds>")
+    if frame and frame:IsShown() and frame.aero.animating then frame.aero.animating = false end
+
+    origToggleBag(id)
+end
+
+local origContainerFrameItemButton_OnEnter = ContainerFrameItemButton_OnEnter
+function ContainerFrameItemButton_OnEnter(button)
+    button = button or this
+    if not button:GetRight() then return end
+
+    origContainerFrameItemButton_OnEnter(button)
+end
+
+-- World map
+Aero:RegisterFrames("WorldMapFrame")
+local mapFrame = CreateFrame("Frame", nil, WorldMapFrame)
+mapFrame:SetAllPoints(WorldMapFrame)
+mapFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+mapFrame:SetScript("OnEvent", function()
+    if WORLDMAP_WINDOWED == 0 then moveFrame(WorldMapFrameTitle, "CENTER", 0, 372) end
+end)
+
+local origWorldMapButton_OnUpdate = WorldMapButton_OnUpdate
+function WorldMapButton_OnUpdate(elapsed)
+    if not this:GetCenter() then return end
+
+    origWorldMapButton_OnUpdate(elapsed)
+end
+
+BlackoutWorld:SetAllPoints(WorldMapFrame)
+WorldMapFrameAreaLabel:SetText("")
+
+-- Turtle WoW
+Aero:RegisterFrames("LFTFrame", "ShopFrame")
+
+local origWorldMapFrame_Minimize = WorldMapFrame_Minimize
+function WorldMapFrame_Minimize()
+    WorldMapContinentDropDown:Hide()
+    WorldMapZoneDropDown:Hide()
+
+    WorldMapFrame.aero = WorldMapFrame.aero or {}
+    WorldMapFrame.aero.animating = true
+    origWorldMapFrame_Minimize()
+    WorldMapFrame.aero.animating = false
+    WorldMapFrame.aero.origScale = WorldMapFrame:GetScale()
+end
+
+local origWorldMapFrame_Maximize = WorldMapFrame_Maximize
+function WorldMapFrame_Maximize()
+    if not WorldMapFrame:IsVisible() then return end
+
+    WorldMapContinentDropDown:Show()
+    WorldMapZoneDropDown:Show()
+
+    WorldMapFrame.aero = WorldMapFrame.aero or {}
+    WorldMapFrame.aero.animating = true
+    origWorldMapFrame_Maximize()
+    WorldMapFrame.aero.animating = false
+    WorldMapFrame.aero.origScale = WorldMapFrame:GetScale()
+
+    moveFrame(WorldMapFrameTitle, "CENTER", 0, 372)
+end
+
+--[[
+Aero:RegisterAddon(addon, ...)
+Use it to register frames that are created after addon loaded or on demand
+Arguments:
+    addon - addon's name
+    ... - either frame names or hook functions, separated by commas
+]]
+
+Aero:RegisterAddon("Blizzard_CraftUI", "CraftFrame")
+Aero:RegisterAddon("Blizzard_BindingUI", "KeyBindingFrame")
+Aero:RegisterAddon("Blizzard_MacroUI", "MacroFrame")
+Aero:RegisterAddon("Blizzard_AuctionUI", "AuctionFrame", "AuctionDressUpFrame")
+Aero:RegisterAddon("Blizzard_GuildBankUI", "GuildBankFrame")
+Aero:RegisterAddon("Blizzard_TalentUI", "TalentFrame")
+Aero:RegisterAddon("Blizzard_TradeSkillUI", "TradeSkillFrame")
+Aero:RegisterAddon("Blizzard_TrainerUI", "ClassTrainerFrame")
+Aero:RegisterAddon("Blizzard_GMSurveyUI", "GMSurveyFrame")
+Aero:RegisterAddon("Blizzard_AchievementUI", "AchievementFrame")
+Aero:RegisterAddon("Blizzard_BattlefieldMinimap", "BattlefieldMinimap")
+Aero:RegisterAddon("Blizzard_ItemSocketingUI", "ItemSocketingFrame")
+Aero:RegisterAddon("TimeManager", "TimeManagerFrame")
+
+-- ShaguTweaks, pfUI and Dragonflight UI
+Aero:RegisterAddon("ShaguTweaks", "AdvancedSettingsGUI")
+Aero:RegisterAddon("Turtle-Dragonflight", "AdvancedSettingsGUI")
+
+local function updateMapScaleAndAlpha()
+    delayRun(0, function()
+        WorldMapFrame.aero.origScale = WorldMapFrame:GetScale()
+
+        local origOnMouseWheel = WorldMapFrame:GetScript("OnMouseWheel")
+        WorldMapFrame:SetScript("OnMouseWheel", function()
+            if origOnMouseWheel then origOnMouseWheel() end
+            if IsShiftKeyDown() then WorldMapFrame.aero.origAlpha = WorldMapFrame:GetAlpha() end
+            if IsControlKeyDown() then WorldMapFrame.aero.origScale = WorldMapFrame:GetScale() end
+        end)
+    end)
+end
+
+local shagu = CreateFrame("Frame")
+shagu:RegisterEvent("PLAYER_ENTERING_WORLD")
+shagu:SetScript("OnEvent", function()
+    if IsAddOnLoaded("ShaguTweaks") then
+        UIErrorsFrame:AddMessage('ShaguTweaks is Loaded')
+        if ShaguTweaks_config[ShaguTweaks.T["WorldMap Window"]] == 1 and WORLDMAP_WINDOWED == 1 then
+            WorldMapFrame_Minimize()
+            delayRun(0, function()
+                WorldMapFrame:SetWidth(720)
+                WorldMapFrame:SetHeight(521)
+            end)
+        end
+        updateMapScaleAndAlpha()
+    elseif IsAddOnLoaded("Turtle-Dragonflight") then
+        UIErrorsFrame:AddMessage('Turtle Dragonflight Loaded')
+        if tDFUI_config[tDFUI.T["WorldMap Window"]] == 1 and WORLDMAP_WINDOWED == 1 then
+            WorldMapFrame_Minimize()
+            delayRun(0, function()
+                WorldMapFrame:SetWidth(720)
+                WorldMapFrame:SetHeight(521)
+            end)
+        end
+        updateMapScaleAndAlpha() 
     end
-end
 
-_G.Aero = Aero
+    if IsAddOnLoaded("pfUI") then
+        Aero:RegisterAddon("pfUI", "pfConfigGUI", "pfAddons")
+        updateMapScaleAndAlpha()
+    end
+
+    shagu:UnregisterEvent("PLAYER_ENTERING_WORLD")
+    shagu:SetScript("OnEvent", nil)
+end)
+L
